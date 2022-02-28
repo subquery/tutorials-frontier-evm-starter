2e24c22a54f621c8b5cd7f97a2ee5c46ad314499
JQQQ <jiqiang90@hotmail.com>
2022-02-28 17:19:54 +1300

    Draft

--- src/chaintypes.ts
+++ src/chaintypes.ts
@@ -1,3 +1,36 @@
-import {typesBundleDeprecated} from "moonbeam-types-bundle"
+import type { OverrideBundleDefinition } from "@polkadot/types/types";
 
-export default {typesBundle: typesBundleDeprecated};
+const definitions: OverrideBundleDefinition = {
+    types: [
+        {
+            // on all versions
+            minmax: [0, undefined],
+            types: {
+                Keys: "AccountId",
+                Address: "MultiAddress",
+                LookupSource: "MultiAddress",
+                AmountOf: "Amount",
+                Amount: "i128",
+                SmartContract: {
+                    _enum: {
+                        Evm: "H160",
+                        Wasm: "AccountId",
+                    },
+                },
+                EraStakingPoints: {
+                    total: "Balance",
+                    stakers: "BTreeMap<AccountId, Balance>",
+                    formerStakedEra: "EraIndex",
+                    claimedRewards: "Balance",
+                },
+                EraRewardAndStake: {
+                    rewards: "Balance",
+                    staked: "Balance",
+                },
+                EraIndex: "u32",
+            },
+        },
+    ],
+};
+
+export default { typesBundle: definitions };
